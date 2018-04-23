b9ae364e9a0637d4006ba2ffe0480867a0b28117
ariffurqon <arif.furqon@gmail.com>
2018-04-23 13:22:29 -0700

    firsty

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        Hi Sexy!
+      </div>
+    );
+  }
+}
+
+export default App
